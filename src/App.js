45a5632b81b0981b217cd48bd8cbac494668ccd2
Tuan Pham <tuanvpham32@outlook.com>
2019-01-27 23:05:46 -0500

    fix typo current user api call

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 
     componentDidMount() {
         if (this.state.logged_in) {
-            fetch('http://localhost:8000/current_user/', {
+            fetch('http://localhost:8000/current-user/', {
                 headers: {
                     Authorization: `JWT ${localStorage.getItem('token')}`
                 }
